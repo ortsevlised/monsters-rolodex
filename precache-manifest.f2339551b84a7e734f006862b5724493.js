977aae21a909f61d8a64c616a67d7cb864d06fc2
Jorge <jdesilvestro@gmail.com>
2020-09-09 19:54:52 +0100

    Updates

---
+++ precache-manifest.f2339551b84a7e734f006862b5724493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4650bbdac8545d791716a81929db0ee5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ea9b8be57c5279bddf8f",
+    "url": "/monsters-rolodex/static/css/main.a4f50b4b.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9b8be57c5279bddf8f",
+    "url": "/monsters-rolodex/static/js/main.ff5374d1.chunk.js"
+  },
+  {
+    "revision": "dfba82531f669c6858b0",
+    "url": "/monsters-rolodex/static/js/runtime-main.589ddcef.js"
+  }
+]);
